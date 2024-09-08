ee4da8658c1fd104925f32cc7e160ebe2c955198
Almaz Karimov <almaz.karimovriddle@mail.ru>
2024-09-08 19:33:20 +0500

    first commit

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php 
-echo "Hello World!";
+
+
+class Example{
+    private $name;
+    public function __construct($name){
+        $this->name = $name;
+    }
+
+    public function show_info(){
+        return "Hello {$this->name}";
+    }
+
+    public function write(){
+        echo $this->show_info();
+    }
+}
+
+class Example2 extends Example{
+    public function __construct($name){
+        parent::__construct($name);   
+    }
+}
+
+$user = new Example2('Almaz Ramilovich');
+$user->write();
